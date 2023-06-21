2a2631f8f679d5f1e761e45f7f8511d70c0bcdad
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-06-21 18:34:49 +0530

    initial deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import { useState } from "react";
-import { initCanvas } from "./util";
 
 function App() {
+  const initCanvas: { color: string; id: number }[][] = [];
+
+  for (let i = 0; i < 10; i++) {
+    const temp: { color: string; id: number }[] = [];
+    for (let j = 0; j < 10; j++) {
+      temp.push({ color: "brown", id: i * 10 + j });
+    }
+    initCanvas.push(temp);
+  }
   const [canvas, setCanvas] = useState(initCanvas);
+  function clickHandler(id: number) {
+    return function () {
+      setCanvas((canvas) => {
+        console.log(id);
+        let j = id % 10;
+        let i = (id - j) / 10;
+        canvas[i][j].color = "white";
+        return canvas;
+      });
+    };
+  }
+
   return (
     <>
       <h1>#paintpixels</h1>
-      {canvas.map((row) => (
-        <div key={row[0].id} style={{ display: "flex" }}>
-          {row.map((unit) => (
-            <div key={unit.id}>{unit.color}</div>
-          ))}
-        </div>
-      ))}
+      <div style={{ border: "solid" }}>
+        {canvas.map((row) => (
+          <div key={row[0].id} style={{ display: "flex" }}>
+            {row.map((unit) => (
+              <div
+                onClick={clickHandler(unit.id)}
+                key={unit.id}
+                style={{
+                  background: unit.color,
+                  width: "50px",
+                  height: "50px",
+                }}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,7 @@ export const initCanvas: { color: string; id: number }[][] = [];
 for (let i = 0; i < 10; i++) {
   const temp: { color: string; id: number }[] = [];
   for (let j = 0; j < 10; j++) {
-    temp.push({ color: "white", id: i * 10 + j });
+    temp.push({ color: "brown", id: i * 10 + j });
   }
   initCanvas.push(temp);
 }
